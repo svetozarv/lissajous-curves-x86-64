255f5d2853c46ee137c52e7a832f428758780ec1
svolskyi <01205787@pw.edu.pl>
2026-05-22 18:17:34 +0200

    .

---
+++ example/f.s
@@ -0,0 +1,21 @@
+section .text
+
+global f
+f:
+    push ebp            ; prolog
+    mov ebp, esp        ; prolog
+    mov eax, [ebp+8]    ; getting the char*
+begin:
+    mov cl, [eax]       ; 1 byte of the char*
+    cmp cl, 0
+    jz end
+
+    add cl, 1
+    mov [eax], cl
+    inc eax
+    jmp begin
+
+end:
+    mov esp, ebp        ; epilog
+    pop ebp             ; epilog
+    ret
