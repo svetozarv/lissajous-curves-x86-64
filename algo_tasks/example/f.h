e7c74d7d74722e3e53bf7b0d5658c29ab47469da
svolskyi <01205787@pw.edu.pl>
2026-05-28 11:25:58 +0200

    .

---
+++ algo_tasks/example/f.h
@@ -0,0 +1,7 @@
+#ifndef F_H_
+#define F_H_
+
+// Capitalize the first character of each word in a string.
+void f(char *s);
+
+#endif  // F_H_
---
+++ algo_tasks/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "f.h"
+
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 4) {
+        printf("Error: Argument missing.\n");
+        printf("Usage: %s <string> <char> <char>\n", argv[0]);
+        return 1;
+    }
+
+    char a = *argv[2];
+    char b = *argv[3];
+    if (a == '\0' || b == '\0') {
+        printf("Invalid argument.");
+        return 1;
+    }
+    printf("%d\n", a);
+    printf("%d\n", b);
+    reversepairs(argv[1]);
+    leavelastndig(argv[1]);
+
+    printf("%s\n", argv[1]);
+    return 0;
+}
