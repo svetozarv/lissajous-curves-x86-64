5dd7e7bf38b4db3ef32cbe7849cfc8924875e59a
svolskyi <01205787@pw.edu.pl>
2026-06-17 01:10:24 +0200

    properly migrate instructions SSE -> AVX-512

--- lissajousAVX512.s
+++ lissajousAVX512.s
@@ -21,121 +21,227 @@ lissajousAVX512:
     push rbp
     mov rbp, rsp
     ; --- prolog ---
-    mov rcx, 250                            ; rcx <- iterations (t <= 1.0 - one rotation; 1 / step_4x = 250 iter) (DO NOT CHANGE)
+    mov rcx, 250                            ; rcx <- iterations (t <= 1.0 - one rotation; 1 / step_4x = 500 iter) (DO NOT CHANGE)
 
-    shufps zmm0, zmm0, 0x00                 ; zmm0 = [ A | A | A | A ]
-    shufps zmm1, zmm1, 0x00                 ; zmm1 = [ B | B | B | B ]
-    shufps zmm2, zmm2, 0x00                 ; zmm2 = [ a | a | a | a ]
-    shufps zmm3, zmm3, 0x00                 ; zmm3 = [ b | b | b | b ]
-    shufps zmm4, zmm4, 0x00                 ; zmm4 = [ delta | delta | delta | delta ]
+    vbroadcastss zmm0, xmm0                 ; copies xmm0[0] to all 8 slots of ymm0 => ymm0 = [ A, A, A, A | A, A, A, A ]
+    vbroadcastss zmm1, xmm1                 ; ymm1 = [ B, B, B, B | B, B, B, B ]
+    vbroadcastss zmm2, xmm2                 ; ymm2 = [ a, a, a, a | a, a, a, a ]
+    vbroadcastss zmm3, xmm3                 ; ymm3 = [ b, b, b, b | b, b, b, b ]
+    vbroadcastss zmm4, xmm4                 ; ymm4 = [ delta, delta, delta, delta | delta, delta, delta, delta ]
 
-    movaps zmm5, [start]                   ; zmm5 <- [ t3 | t2 | t1 | t0 ], future [ x3 | x2 | x1 | x0 ]
-    movaps zmm6, zmm5                       ; zmm6 <- [ t3 | t2 | t1 | t0 ], future [ y3 | y2 | y1 | y0 ]
-    movaps zmm15, zmm5                      ; zmm15 <- [ curr_t | curr_t | curr_t | curr_t ] = [ t3 | t2 | t1 | t0 ]
-    movaps zmm14, [step_4x]                 ; zmm14 <- [ 0.004 | 0.004 | 0.004 | 0.004 ]
+    vmovaps zmm5, [start]                   ; ymm5 <- [ t3 | t2 | t1 | t0 ], future [ x3 | x2 | x1 | x0 ]
+    vmovaps zmm6, zmm5                       ; ymm6 <- [ t3 | t2 | t1 | t0 ], future [ y3 | y2 | y1 | y0 ]
+    vmovaps zmm15, zmm5                      ; ymm15 <- [ curr_t | curr_t | curr_t | curr_t ] = [ t3 | t2 | t1 | t0 ]
+    vmovaps zmm14, [step_4x]                 ; ymm14 <- [ 0.004 | 0.004 | 0.004 | 0.004 ]
 
-    cvtsi2ss zmm9, esi                      ; zmm9 = [ 0 | 0 | 0 | Width ]
-    shufps zmm9, zmm9, 0x00                 ; [ Width | Width | Width | Width ]
-    mulps zmm9, [half]                      ; zmm14 = [ 0.5*Width | 0.5*Width | 0.5*Width | 0.5*Width ]
+    vcvtsi2ss xmm9, xmm9, esi                      ; ymm9 = [ 0 | 0 | 0 | Width ]
+    vbroadcastss zmm9, xmm9                 ; [ Width | Width | Width | Width ]
+    vmulps zmm9, zmm9, [half]                      ; ymm14 = [ 0.5*Width | 0.5*Width | 0.5*Width | 0.5*Width ]
 
-    cvtsi2ss zmm10, edx                     ; zmm15 = [ 0 | 0 | 0 | Height ]
-    shufps   zmm10, zmm10, 0x00             ; [ Height | Height | Height | Height ]
-    mulps    zmm10, [half]                  ; zmm15 = [ 0.5*Height | 0.5*Height | 0.5*Height | 0.5*Height ]
+    vcvtsi2ss xmm10, xmm10, edx                     ; ymm15 = [ 0 | 0 | 0 | Height ]
+    vbroadcastss   xmm10, xmm10             ; [ Height | Height | Height | Height ]
+    vmulps    zmm10, zmm10, [half]                  ; ymm15 = [ 0.5*Height | 0.5*Height | 0.5*Height | 0.5*Height ]
 
 mainloop:
-    ; ====== compute x(t) ======
-    mulps zmm5, zmm2                        ; zmm5 = [ a*t3 | a*t2 | a*t1 | a*t0 ]
-    addps zmm5, zmm4                        ; zmm5 = [ a*t3+delta | a*t2+delta | a*t1+delta | a*t0+delta ]
+    ; ================== compute x(t) ==================
+    vmulps zmm5, zmm5, zmm2                        ; ymm5 = [ a*t3 | a*t2 | a*t1 | a*t0 ]
+    vaddps zmm5, zmm5, zmm4                        ; ymm5 = [ a*t3+delta | a*t2+delta | a*t1+delta | a*t0+delta ]
 
     ; normalize z := a*t+delta for sine
-    movaps zmm7, zmm5
-    roundps zmm7, zmm7, 0x00
-    subps zmm5, zmm7
+    vmovaps zmm7, zmm5
+    vroundps zmm7, zmm7, 0x00
+    vsubps zmm5, zmm5, zmm7
 
     ; compute sine
-    movaps zmm7, zmm5                       ; x = zmm5
-    mulps zmm7, zmm7                        ; x^2 = zmm7
-    movaps zmm8, [c9]
-    mulps zmm8, zmm7
-    addps zmm8, [c7]
-    mulps zmm8, zmm7
-    addps zmm8, [c5]
-    mulps zmm8, zmm7
-    addps zmm8, [c3]
-    mulps zmm8, zmm7
-    addps zmm8, [c1]
-    mulps zmm8, zmm5
-    movaps zmm5, zmm8
-    ; zmm5 = [ sin(a*t3+delta) | sin(a*t2+delta) | sin(a*t1+delta) | sin(a*t0+delta) ]
+    vmovaps zmm7, zmm5                       ; x = ymm5
+    vmulps zmm7, zmm7, zmm7                        ; x^2 = ymm7
+    vmovaps zmm8, [c9]
+    vmulps zmm8, zmm8, zmm7
+    vaddps zmm8, zmm8, [c7]
+    vmulps zmm8, zmm8, zmm7
+    vaddps zmm8, zmm8, [c5]
+    vmulps zmm8, zmm8, zmm7
+    vaddps zmm8, zmm8, [c3]
+    vmulps zmm8, zmm8, zmm7
+    vaddps zmm8, zmm8, [c1]
+    vmulps zmm8, zmm8, zmm5
+    vmovaps zmm5, zmm8
+    ; ymm5 = [ sin(a*t3+delta) | sin(a*t2+delta) | sin(a*t1+delta) | sin(a*t0+delta) ]
 
-    mulps zmm5, zmm0                        ; zmm5 = [ A*sin(a*t3+delta) | A*sin(a*t2+delta) | A*sin(a*t1+delta) | A*sin(a*t0+delta) ]
+    vmulps zmm5, zmm5, zmm0                        ; ymm5 = [ A*sin(a*t3+delta) | A*sin(a*t2+delta) | A*sin(a*t1+delta) | A*sin(a*t0+delta) ]
 
 
-    ; ====== compute y(t) ======
-    mulps zmm6, zmm3                        ; zmm6 = [ b*t3 | b*t2 | b*t1 | b*t0 ]
+    ; ================== compute y(t) ==================
+    vmulps zmm6, zmm6, zmm3                        ; ymm6 = [ b*t3 | b*t2 | b*t1 | b*t0 ]
 
     ; normalize z := b*t for sine
-    movaps zmm7, zmm6
-    roundps zmm7, zmm7, 0x00
-    subps zmm6, zmm7
+    vmovaps zmm7, zmm6
+    vroundps zmm7, zmm7, 0x00
+    vsubps zmm6, zmm6, zmm7
 
     ; compute sine
-    movaps zmm7, zmm6                       ; y = zmm6
-    mulps zmm7, zmm7                        ; y^2 = zmm7
-    movaps zmm8, [c9]
-    mulps zmm8, zmm7
-    addps zmm8, [c7]
-    mulps zmm8, zmm7
-    addps zmm8, [c5]
-    mulps zmm8, zmm7
-    addps zmm8, [c3]
-    mulps zmm8, zmm7
-    addps zmm8, [c1]
-    mulps zmm8, zmm6
-    movaps zmm6, zmm8
-    ; zmm6 = [ sin(b*t3) | sin(b*t2) | sin(b*t1) | sin(b*t0) ]
+    vmovaps zmm7, zmm6                       ; y = ymm6
+    vmulps zmm7, zmm7, zmm7                        ; y^2 = ymm7
+    vmovaps zmm8, [c9]
+    vmulps zmm8, zmm8, zmm7
+    vaddps zmm8, zmm8, [c7]
+    vmulps zmm8, zmm8, zmm7
+    vaddps zmm8, zmm8, [c5]
+    vmulps zmm8, zmm8, zmm7
+    vaddps zmm8, zmm8, [c3]
+    vmulps zmm8, zmm8, zmm7
+    vaddps zmm8, zmm8, [c1]
+    vmulps zmm8, zmm8, zmm6
+    vmovaps zmm6, zmm8
+    ; ymm6 = [ sin(b*t3) | sin(b*t2) | sin(b*t1) | sin(b*t0) ]
 
-    mulps zmm6, zmm1                        ; zmm6 = [ B*sin(b*t3) | B*sin(b*t2) | B*sin(b*t1) | B*sin(b*t0) ]
+    vmulps zmm6, zmm6, zmm1                        ; ymm6 = [ B*sin(b*t3) | B*sin(b*t2) | B*sin(b*t1) | B*sin(b*t0) ]
 
-    addps zmm5, zmm9                        ; x = A*sin(a*t+delta) + 0.5*Width
-    addps zmm6, zmm10                       ; y = B*sin(b*t) + 0.5*Height
+    vaddps zmm5, zmm5, zmm9                        ; x = A*sin(a*t+delta) + 0.5*Width
+    vaddps zmm6, zmm6, zmm10                       ; y = B*sin(b*t) + 0.5*Height
 
-    ; ====== draw points ======
-    cvttps2dq zmm5, zmm5                    ; convert x to int
-    cvttps2dq zmm6, zmm6                    ; convert y to int
-    pextrd r8d, zmm5, 0                     ; r8d = x0
-    pextrd r9d, zmm6, 0                     ; r9d = y0
+    ; ================== draw points ==================
+    vcvttps2dq zmm5, zmm5                    ; convert x to int
+    vcvttps2dq zmm6, zmm6                    ; convert y to int
+    vpextrd r8d, xmm5, 0                     ; r8d = x0
+    vpextrd r9d, xmm6, 0                     ; r9d = y0
     mov eax, r9d                            ; y
     imul eax, esi                           ; y*width
     add eax, r8d                            ; y*width + x
     mov dword [rdi + 4*rax], 0xFFFFFFFF     ; set pixel (x0, y0) to white
 
-    pextrd r8d, zmm5, 1                     ; r8d = x1
-    pextrd r9d, zmm6, 1                     ; r9d = y1
+    vpextrd r8d, xmm5, 1                     ; r8d = x1
+    vpextrd r9d, xmm6, 1                     ; r9d = y1
     mov eax, r9d
     imul eax, esi
     add eax, r8d
     mov dword [rdi + 4*rax], 0xFFFFFFFF     ; set pixel (x0, y0) to white
 
-    pextrd r8d, zmm5, 2                     ; r8d = x2
-    pextrd r9d, zmm6, 2                     ; r9d = y2
+    vpextrd r8d, xmm5, 2                     ; r8d = x2
+    vpextrd r9d, xmm6, 2                     ; r9d = y2
     mov eax, r9d
     imul eax, esi
     add eax, r8d
     mov dword [rdi + 4*rax], 0xFFFFFFFF     ; set pixel (x0, y0) to white
 
-    pextrd r8d, zmm5, 3                     ; r8d = x3
-    pextrd r9d, zmm6, 3                     ; r9d = y3
+    vpextrd r8d, xmm5, 3                     ; r8d = x3
+    vpextrd r9d, xmm6, 3                     ; r9d = y3
     mov eax, r9d
     imul eax, esi
     add eax, r8d
     mov dword [rdi + 4*rax], 0xFFFFFFFF     ; set pixel (x0, y0) to white
+
+    vextractf32x4 xmm12, zmm5, 1             ; xmm11 = [ x7 | x6 | x5 | x4 ]
+    vextractf32x4 xmm12, zmm6, 1             ; xmm12 = [ y7 | y6 | y5 | y4 ]
+
+    ; 5 pixel
+    vpextrd r8d, xmm11, 0                   ; r8d = x4
+    vpextrd r9d, xmm12, 0                   ; r9d = y4
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
+
+    ; 6 pixel
+    vpextrd r8d, xmm11, 1                   ; r8d = x5
+    vpextrd r9d, xmm12, 1                   ; r9d = y5
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
+
+    ; 7 pixel
+    vpextrd r8d, xmm11, 2                   ; r8d = x6
+    vpextrd r9d, xmm12, 2                   ; r9d = y6
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
+
+    ; 8 pixel
+    vpextrd r8d, xmm11, 3                   ; r8d = x7
+    vpextrd r9d, xmm12, 3                   ; r9d = y7
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
+
+
+    vextractf32x4 xmm12, zmm5, 2             ; xmm11 = [ x7 | x6 | x5 | x4 ]
+    vextractf32x4 xmm12, zmm6, 2             ; xmm12 = [ y7 | y6 | y5 | y4 ]
+
+    ; 9 pixel
+    vpextrd r8d, xmm11, 0                   ; r8d = x4
+    vpextrd r9d, xmm12, 0                   ; r9d = y4
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
+
+    ; 10 pixel
+    vpextrd r8d, xmm11, 1                   ; r8d = x5
+    vpextrd r9d, xmm12, 1                   ; r9d = y5
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
+
+    ; 11 pixel
+    vpextrd r8d, xmm11, 2                   ; r8d = x6
+    vpextrd r9d, xmm12, 2                   ; r9d = y6
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
+
+    ; 12 pixel
+    vpextrd r8d, xmm11, 3                   ; r8d = x7
+    vpextrd r9d, xmm12, 3                   ; r9d = y7
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
+
+    vextractf32x4 xmm12, zmm5, 3             ; xmm11 = [ x7 | x6 | x5 | x4 ]
+    vextractf32x4 xmm12, zmm6, 3             ; xmm12 = [ y7 | y6 | y5 | y4 ]
+
+    ; 13 pixel
+    vpextrd r8d, xmm11, 0                   ; r8d = x4
+    vpextrd r9d, xmm12, 0                   ; r9d = y4
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
+
+    ; 14 pixel
+    vpextrd r8d, xmm11, 1                   ; r8d = x5
+    vpextrd r9d, xmm12, 1                   ; r9d = y5
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
+
+    ; 15 pixel
+    vpextrd r8d, xmm11, 2                   ; r8d = x6
+    vpextrd r9d, xmm12, 2                   ; r9d = y6
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
+
+    ; 16 pixel
+    vpextrd r8d, xmm11, 3                   ; r8d = x7
+    vpextrd r9d, xmm12, 3                   ; r9d = y7
+    mov eax, r9d
+    imul eax, esi
+    add eax, r8d
+    mov dword [rdi + 4*rax], 0xFFFFFFFF
 
 
     ; ====== update t ======
-    addps zmm15, zmm14                      ; t += 0.004
-    movaps zmm5, zmm15                      ; current t
-    movaps zmm6, zmm15                      ; current t
+    vaddps ymm15, ymm15, ymm14                      ; t += 0.004
+    vmovaps ymm5, ymm15                      ; current t
+    vmovaps ymm6, ymm15                      ; current t
 
     dec rcx
     test rcx, rcx
